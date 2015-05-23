059709f8c68c232947c5361feed9c70481e152c0
Malte <malte@test.de>
2015-05-23 20:08:01 +0200

    Web-Client Use-Cases definiert

--- app/controllers/webclient_controller.rb
+++ app/controllers/webclient_controller.rb
@@ -1,6 +1,11 @@
 
 
 require 'rest-client'
+require 'pbkdf2'
+require 'openssl'
+require 'base64'
+require 'rubygems'
+require 'rest_client'
 
 class WebclientController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
@@ -8,22 +13,91 @@ class WebclientController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  require 'rubygems'
-  require 'rest_client'
+
+  # Client Variablen
+  @Identity
+  @salt_masterkey
+  @pubkey_user
+  @privkey_user_enc
+  @privkey_user
+  @timestamp
+  @Recipient
+  @sig_service
+  @cipher
+  @iv
+  @key_recipient_enc
+  @sig_recipient
+  @Signature
+  @password
+
 
 
   def register
 
+    # Erhalte PubKey vom Server
+
     response =RestClient.create 'http://fh.thomassennekamp.de/server/user' , {:params => {:identity => 'TestWEB',:salt_masterkey => 12345679876543,:pubkey_user => 123456765432,:privkey_user_enc => 123456765432 }}
-    response.code
+
+
+
   end
 
 
-  def getInfo
-    RestClient.get 'http://fh.thomassennekamp.de/server/User', {:params => {:identity => 'thomas06'}}
+
+  def login
+
+    # Erhalte PubKey vom Server
+    response = RestClient.get 'http://fh.thomassennekamp.de/server/User', {:params => {:identity => 'thomas06'}}
    # RestClient::NotImplemented: 501 Not Implemen
+    # Pubkey User auslesen
+    @pubkey_user = response['pubkey_user'].decode64
+    @salt_masterkey = response['salt_masterkey'].decode64
+    @privkey_user_enc = response['privkey_user_enc'].decode64
+    # Masterkey genieren mit PDKDF default: sha 256
+
+    @masterkey = PBKDF2.new do |p|
+                    p.password = @password
+                    p.salt = @salt_masterkey
+                    p.iterations = 10000
+                  end
 
 
+    #   Entschlüsslung privkey_user_enc
+
+
+      @KEY = @masterkey
+      @ALGORITHM = 'AES-128-ECB'
+
+      def self.encryption(msg)
+        begin
+          cipher = OpenSSL::Cipher.new(@ALGORITHM)
+          cipher.encrypt()
+          cipher.key = KEY
+          crypt = cipher.update(msg) + cipher.final()
+          crypt_string = (Base64.encode64(crypt))
+          return crypt_string
+        rescue Exception => exc
+          puts ('Message for the encryption log file for message #{msg} = #{exc.message}')
+        end
+      end
+      def self.decryption(msg)
+        begin
+          cipher = OpenSSL::Cipher.new(ALGORITHM)
+          cipher.decrypt()
+          cipher.key = KEY
+          tempkey = Base64.decode64(msg)
+          crypt = cipher.update(tempkey)
+          crypt << cipher.final()
+          return crypt
+          rescue Exception => exc
+          puts ('Message for the decryption log file : message#{msg} = #{exc.message}')
+        end
+      end
+
+    # Aufruf zur Entschlüssung
+    @privkey_user = AesEncryptDecrypt.encryption(@privkey_user_enc)
+
+    end
   end
 
 
